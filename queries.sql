0012866700aa0132a49564900dd2e42e7051d256
Ferb-10 <fm2210to@gmail.com>
2026-01-18 20:05:16 +0900

    modify queries

--- queries.sql
+++ queries.sql
@@ -2,11 +2,12 @@ CREATE TABLE books (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   title TEXT NOT NULL,
   author TEXT NOT NULL,
-  isbn TEXT UNIQUE,
+  isbn TEXT,
   cover_url TEXT,
   status TEXT NOT NULL CHECK(status IN ('want', 'finished')),
   review INTEGER CHECK(review BETWEEN 1 AND 5),
-  comment TEXT,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  finished_at DATETIME
+  comment TEXT
 );
+
+
+
